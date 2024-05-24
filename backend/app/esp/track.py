4450072e652174327d5c145785bad57bc4ec53e1
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2024-05-24 14:37:51 +0630

    complete track class for incoming validation from controller

--- backend/app/esp/track.py
+++ backend/app/esp/track.py
@@ -1,5 +1,7 @@
 from datetime import datetime 
 from ..models import *
+from werkzeug.security import check_password_hash
+from flask import current_app
 
 class Track:
     def __init__(self, who: str) -> None:
@@ -12,11 +14,14 @@ class Track:
         """        
         current = datetime.now().date()
         current_date = datetime(current.year, current.month, current.day)
-        time = Time.query.filter_by(date=exist_date).first()
-        if time:
-            if( current_date == exist_date ):
-                return True
-        return False    
+        try:
+            time = Time.query.filter_by(date=exist_date).first()
+            if time:
+                if( current_date == exist_date ):
+                    return True
+        except Exception as e:
+            current_app.logger.error(e)
+            return False    
     
     def __get_object(self):
         match self.who:
@@ -29,62 +34,123 @@ class Track:
             case "guest":
                 return Guest
             case _:
-                raise None
+                return None
     
+    def __set_id_to_time(self, time_obj: Time, id: int)->None:
+        match self.who:
+            case "student":
+                time_obj.student_id = id
+            case "teacher":
+                time_obj.teacher_id = id
+            case "staff":
+                time_obj.staff_id = id
+            case "guest":
+                time_obj.guest_id = id
+            case _:
+                return None
+    
+    def __get_id_from_people(self, people: object) -> int:
+        match self.who:
+            case "student":
+                return people.student_id
+            case "teacher":
+                return people.teacher_id
+            case "staff":
+                return people.staff_id
+            case "guest":
+                return people.guest_id
+            case _:
+                return None
     def __passed_today(self, id: int) -> TrackPass:
         """
         get id as args and check is already today passed 
         return exist trackpass object or new trackpass
         """
-        track_pass = TrackPass.query.filter_by(**{
+        try:
+            track_pass = TrackPass.query.filter_by(**{
             f"{self.who}_id": id,
             "date": datetime.now().date()
             }).first()
-        if track_pass:
-            return track_pass
+            if track_pass:
+                return track_pass
+        except Exception as e:
+            current_app.logger.error(e)
+            return False
         return TrackPass()
     
-    def __check(self, unique_id: str)->bool:
-        people = self.__get_object().query.filter_by(**{f"{self.who}_id": unique_id}).first()
-        if people:
-            pass
+    def __check(self, unique_id: str, datas: list )->bool:
+        try:
+            people = self.__get_object().query.filter_by(**{f"{self.who}_id": unique_id}).first()
+            if people:
+                formatted_data = self.__get_format(people=people)
+                if check_password_hash(formatted_data, formatted_data):
+                    today_passed_people =self.__passed_today(unique_id)
+                    if today_passed_people:
+                        if (len(today_passed_people.times) + 1) % 2 != 0:
+                            in_time = Time()
+                            in_time.in_time = datetime.now()
+                            in_time.pass_id = today_passed_people.pass_id
+                            db.session.add(in_time)
+                            db.session.commit()
+                            return True
+                        out_time = Time()
+                        out_time.out_time = datetime.now()
+                        out_time.pass_id = today_passed_people.pass_id
+                        db.session.add(out_time)
+                        db.session.commit()
+                        return True
+                    # create new in coming pass
+                    new_in_pass_people = TrackPass()
+                    self.__set_id_to_time(new_in_pass_people, self.__get_id_from_people(people=people))
+                    db.session.add(new_in_pass_people)
+                    db.session.commit()
+                    # create new in time data 
+                    new_in_pass_time = Time()
+                    new_in_pass_time.in_time = datetime.now()
+                    new_in_pass_time.pass_id = new_in_pass_people.pass_id
+                    db.session.add(new_in_pass_time)
+                    db.session.commit()
+                    return True
+        except Exception as e:
+            current_app.logger.error(e)
+            return False
         
     def add_pass(self, datas: dict) -> bool:
         pass
     
     
-    def __get_format(self, datas: list)->dict:
+    def __get_format(self, people: object)->dict:
         match self.who:
             case "student":
                 return {
-                    "student_id": datas[0],
-                    "name": datas[1],
-                    "roll_no": datas[2],
-                    "father_name": datas[3],
-                    "current_semester": datas[4] 
+                    "student_id": people.student_id,
+                    "name": people.name,
+                    "roll_no": people.roll_no,
+                    "father_name": people.father_name,
+                    "current_semester": people.current_semester
                 }
             case "teacher":
                 return {
-                    "teacher_id": datas[0],
-                    "name": datas[1],
-                    "department": datas[2],
-                    "position": datas[3],
-                    "nrc": datas[4],
-                    "birth_date": datas[5]
+                    "teacher_id": people.teacher_id,
+                    "name": people.name,
+                    "department": people.department,
+                    "position": people.position,
+                    "nrc": people.nrc,
+                    "birth_date": people.birth_date
                 }
             case "staff":
                 return {
-                    "staff_id": datas[0],
-                    "name": datas[1],
-                    "position": datas[2],
-                    "birth_date": datas[3]
+                    "staff_id": people.staff_id,
+                    "name": people.name,
+                    "position": people.position,
+                    "birth_date": people.birth_date
                 }
             case "guest":
                 return {
-                    "guest_id": datas[0],
-                    "name": datas[1],
-                    "token": datas[2],
-                    "register_date": datas[3]
+                    "guest_id": people.guest_id,
+                    "name": people.name,
+                    "token": people.token,
+                    "register_date": people.register_date
                 }
                 
             case _:
